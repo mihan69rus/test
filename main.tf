327b7ea4f9bf0bd14a3ec54e8ecee408811d1cf7
mihan69rus <36642761+mihan69rus@users.noreply.github.com>
2025-03-19 23:41:11 +0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3.9"
+}
+
+variable "subject" {
+   type = string
+   default = "World"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "Hello TF Controller v0.16.0-rc.2, ${var.subject}!"
+}
